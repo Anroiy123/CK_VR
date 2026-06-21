7eae33570cf73d17874774d9d2dd315bb72b0a3b
Anroiy123 <hunganroiy123@gmail.com>
2026-06-21 22:41:19 +0700

    switch bg music

--- js/sound-manager.js
+++ js/sound-manager.js
@@ -58,30 +58,25 @@
     tick: {
       volume: 0.055,
       wave: "square",
-      notes: [
-        { frequency: 880, start: 0, duration: 0.035 },
-      ],
+      notes: [{ frequency: 880, start: 0, duration: 0.035 }],
     },
   };
-
-  const MUSIC_PATTERN = [
-    { bass: 130.81, chord: [261.63, 329.63, 392.0] },
-    { bass: 174.61, chord: [349.23, 440.0, 523.25] },
-    { bass: 196.0, chord: [392.0, 493.88, 587.33] },
-    { bass: 164.81, chord: [329.63, 392.0, 493.88] },
-  ];
 
   const SoundManager = {
     audioContext: null,
     masterGain: null,
-    musicGain: null,
     effectsGain: null,
-    bgmTimer: null,
-    bgmStep: 0,
+    backgroundMusic: null,
     bgmEnabled: false,
     ready: false,
 
     init: function init() {
+      this.backgroundMusic = new Audio(
+        "assets/audio/Wii Shop Bossa Nova Cover.mp3",
+      );
+      this.backgroundMusic.loop = true;
+      this.backgroundMusic.preload = "auto";
+      this.backgroundMusic.volume = 0.1;
       this.ready = true;
       this.bindUnlockListeners();
     },
@@ -91,8 +86,8 @@
       var unlock = function unlock() {
         self.ensureAudioGraph();
         self.resumeContext();
-        if (self.bgmEnabled && !self.bgmTimer) {
-          self.startProceduralBGM();
+        if (self.bgmEnabled) {
+          self.playBackgroundMusic();
         }
         document.removeEventListener("click", unlock);
         document.removeEventListener("keydown", unlock);
@@ -115,14 +110,11 @@
 
       this.audioContext = new AudioContextCtor();
       this.masterGain = this.audioContext.createGain();
-      this.musicGain = this.audioContext.createGain();
       this.effectsGain = this.audioContext.createGain();
 
       this.masterGain.gain.value = 0.82;
-      this.musicGain.gain.value = 0.18;
       this.effectsGain.gain.value = 0.62;
 
-      this.musicGain.connect(this.masterGain);
       this.effectsGain.connect(this.masterGain);
       this.masterGain.connect(this.audioContext.destination);
       return true;
@@ -146,21 +138,25 @@
 
     startBGM: function startBGM() {
       this.bgmEnabled = true;
-      if (!this.ensureAudioGraph()) {
-        return;
-      }
-
-      this.resumeContext();
-      if (!this.bgmTimer) {
-        this.startProceduralBGM();
-      }
+      this.playBackgroundMusic();
     },
 
     stopBGM: function stopBGM() {
       this.bgmEnabled = false;
-      if (this.bgmTimer) {
-        clearInterval(this.bgmTimer);
-        this.bgmTimer = null;
+      if (this.backgroundMusic) {
+        this.backgroundMusic.pause();
+        this.backgroundMusic.currentTime = 0;
+      }
+    },
+
+    playBackgroundMusic: function playBackgroundMusic() {
+      if (!this.backgroundMusic || !this.bgmEnabled) {
+        return;
+      }
+
+      var playPromise = this.backgroundMusic.play();
+      if (playPromise && typeof playPromise.catch === "function") {
+        playPromise.catch(function () {});
       }
     },
 
@@ -178,7 +174,10 @@
         oscillator.type = definition.wave;
         oscillator.frequency.setValueAtTime(note.frequency, startAt);
         gain.gain.setValueAtTime(0.0001, startAt);
-        gain.gain.exponentialRampToValueAtTime(definition.volume, startAt + 0.012);
+        gain.gain.exponentialRampToValueAtTime(
+          definition.volume,
+          startAt + 0.012,
+        );
         gain.gain.exponentialRampToValueAtTime(0.0001, endAt);
 
         oscillator.connect(gain);
@@ -186,51 +185,6 @@
         oscillator.start(startAt);
         oscillator.stop(endAt + 0.025);
       });
-    },
-
-    startProceduralBGM: function startProceduralBGM() {
-      var self = this;
-      this.playMusicStep();
-      this.bgmTimer = setInterval(function () {
-        self.playMusicStep();
-      }, 1800);
-    },
-
-    playMusicStep: function playMusicStep() {
-      if (!this.bgmEnabled || !this.audioContext || !this.musicGain) {
-        return;
-      }
-
-      var context = this.audioContext;
-      var step = MUSIC_PATTERN[this.bgmStep % MUSIC_PATTERN.length];
-      var startAt = context.currentTime + 0.02;
-
-      this.playMusicNote(step.bass, startAt, 1.55, "sine", 0.18);
-      step.chord.forEach(
-        function (frequency, index) {
-          this.playMusicNote(frequency, startAt + index * 0.04, 1.35, "triangle", 0.055);
-        }.bind(this),
-      );
-
-      this.bgmStep += 1;
-    },
-
-    playMusicNote: function playMusicNote(frequency, startAt, duration, wave, volume) {
-      var context = this.audioContext;
-      var oscillator = context.createOscillator();
-      var gain = context.createGain();
-      var endAt = startAt + duration;
-
-      oscillator.type = wave;
-      oscillator.frequency.setValueAtTime(frequency, startAt);
-      gain.gain.setValueAtTime(0.0001, startAt);
-      gain.gain.exponentialRampToValueAtTime(volume, startAt + 0.08);
-      gain.gain.exponentialRampToValueAtTime(0.0001, endAt);
-
-      oscillator.connect(gain);
-      gain.connect(this.musicGain);
-      oscillator.start(startAt);
-      oscillator.stop(endAt + 0.05);
     },
   };
 
